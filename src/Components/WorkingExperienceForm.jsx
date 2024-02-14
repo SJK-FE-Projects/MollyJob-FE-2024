98173e0c047acd3013de21470dffaaa4a52952aa
mirJakub <slawomir.krzyzak@gmail.com>
2024-02-14 13:44:35 +0100

    forms

---
+++ src/Components/WorkingExperienceForm.jsx
@@ -0,0 +1,92 @@
+import { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../Context/AuthContext";
+
+
+
+const AuthForm = ({ isLogin = true }) => {
+  const [jobType, setJobType] = useState("");
+  const [jobRole, setJobRole] = useState("");
+  const [company, setCompany] = useState("");
+
+  const navigate = useNavigate();
+  const { saveToken } = useContext(AuthContext);
+
+  const handleJobType = (event) => setJobType(event.target.value);
+  const handleJobRole = (event) => setJobRole(event.target.value);
+  const handleCompany = (event) => setCompany(event.target.value);
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const reqBody = {jobType,jobRole,company};
+
+    // try {
+    //   const response = await fetch(
+    //     // Do we need to add any url in the .env file ??????
+
+    //     `${import.meta.env.VITE_API_URL}/auth/${isLogin ? "login" : "signup"}`,
+    //     {
+    //       method: "POST",
+    //       headers: { "Content-Type": "application/json" },
+    //       body: JSON.stringify(reqBody),
+    //     }
+    //   );
+
+    //   console.log("Response status:", response.status);
+
+    //   if (response.status === 201) {
+    //     // The user was created successully
+    //     navigate("/login");
+    //   }
+    //   if (response.status === 200) {
+    //     // The user was logged in successully
+    //     const parsed = await response.json();
+    //     console.log(parsed);
+    //     alert("User logged in successfully")
+    //     navigate("/");
+
+    //     saveToken(parsed.token);
+    //   }
+    // } catch (error) {
+    //   console.log(error);
+    // }
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+       
+          <label>
+            Job Type
+            <input
+              type="text"
+              required
+              value={jobType}
+              onChange={handleJobType}
+            />
+          </label>
+          <label>
+            Job Role
+            <input
+              type="text"
+              required
+              value={jobRole}
+              onChange={handleJobRole}
+            />
+          </label>
+          <label>
+            Company
+            <input
+              type="text"
+              required
+              value={company}
+              onChange={handleCompany}
+            />
+          </label>
+
+        <button mt="xl" variant="filled" bg={theme.colors.dark[1]} size="xs" radius="xl" type="submit">{isLogin ? "Login" : "Signup"}</button>
+      </form>
+    </>
+  );
+};
+
+export default AuthForm;
