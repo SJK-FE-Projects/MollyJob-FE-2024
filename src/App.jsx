6684e0d502e1b42f646536da02874ad42fb77e4e
sonali582000 <rathodsonali393@gmail.com>
2024-02-21 18:22:22 +0100

    worked on signup and login

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,33 @@
 import { Route, Routes } from "react-router-dom";
-import WelcomeSplitScreen from './Pages/WelcomeSplitScreen';
-import LoginPage from './Pages/LoginPage'
-import CoverLetterFormWorker from './Pages/CoverLetterFormCoWorker'
-import CoverLetterFormStudent from './Pages/CoverLetterFormStudent'
+import WelcomeSplitScreen from "./Pages/WelcomeSplitScreen";
+import CoverLetterFormWorker from "./Pages/CoverLetterFormCoWorker";
+import CoverLetterFormStudent from "./Pages/CoverLetterFormStudent";
 
-
-import styles from './Styles/App.module.scss'
-import Navigation from './Components/Navigation'
+import styles from "./Styles/App.module.scss";
+import Navigation from "./Components/Navigation";
+import SignUpPage from "./Pages/SingUpPage";
+import LoginPage from "./Pages/LoginPage";
 
 function App() {
-
   return (
     <div className={styles.App}>
       <Routes>
-        <Route path='/' element={<WelcomeSplitScreen/>} />
-        <Route path='student/login' element={<LoginPage/>} />
-        <Route path='coworker/login' element={<LoginPage/>} />
-        <Route path='coworker/coverletterform' element={<CoverLetterFormWorker/>} />
-        <Route path='student/coverletterform' element={<CoverLetterFormStudent/>} />
+        <Route path="/" element={<WelcomeSplitScreen />} />
+        <Route path="signup/" element={<SignUpPage />} />
+        <Route path="login/" element={<LoginPage />} />
+
+        <Route
+          path="coworker/coverletterform"
+          element={<CoverLetterFormWorker />}
+        />
+        <Route
+          path="student/coverletterform"
+          element={<CoverLetterFormStudent />}
+        />
       </Routes>
-      <Navigation/>
+      <Navigation />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
